602f2ef86155b0942f58c72d01ca7499eab684c6
JimDong <jimdong@damaiapp.com.tw>
2024-04-12 18:05:34 +0800

    seed

---
+++ prisma/migrations/20240412090337_2/migration.sql
@@ -0,0 +1,61 @@
+-- CreateTable
+CREATE TABLE "Creator" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "imgUrl" TEXT NOT NULL,
+
+    CONSTRAINT "Creator_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Video" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "imgUrl" TEXT NOT NULL,
+    "creatorId" INTEGER NOT NULL,
+
+    CONSTRAINT "Video_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Comment" (
+    "id" SERIAL NOT NULL,
+    "content" TEXT NOT NULL,
+    "senderId" INTEGER NOT NULL,
+    "videoId" INTEGER NOT NULL,
+    "parentId" INTEGER,
+
+    CONSTRAINT "Comment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_CreatorToUser" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_CreatorToUser_AB_unique" ON "_CreatorToUser"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_CreatorToUser_B_index" ON "_CreatorToUser"("B");
+
+-- AddForeignKey
+ALTER TABLE "Video" ADD CONSTRAINT "Video_creatorId_fkey" FOREIGN KEY ("creatorId") REFERENCES "Creator"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_senderId_fkey" FOREIGN KEY ("senderId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_videoId_fkey" FOREIGN KEY ("videoId") REFERENCES "Video"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Comment"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CreatorToUser" ADD CONSTRAINT "_CreatorToUser_A_fkey" FOREIGN KEY ("A") REFERENCES "Creator"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CreatorToUser" ADD CONSTRAINT "_CreatorToUser_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ prisma/seed.ts
@@ -0,0 +1,125 @@
+// 這邊要做的就是 拿到orm 工具，用該工具塞資料，結束
+
+import { PrismaClient, Prisma } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const authInput: Prisma.AuthCreateInput = {
+    maxCreatorNum: 5,
+  };
+
+  const auth1 = await prisma.auth.upsert({
+    where: { id: 1 },
+    update: {
+      maxCreatorNum: 100,
+    },
+    create: {
+      id: 1,
+      maxCreatorNum: 5,
+    },
+  });
+
+  const user1 = await prisma.user.upsert({
+    where: { id: 1 },
+    update: {},
+    create: {
+      id: 1,
+      name: '澤東',
+      password: '123',
+      authId: auth1.id,
+    },
+  });
+
+  const user2 = await prisma.user.upsert({
+    where: { id: 2 },
+    update: {},
+    create: {
+      id: 2,
+      name: '近平',
+      password: '64',
+      authId: auth1.id,
+    },
+  });
+
+  const creator1 = await prisma.creator.upsert({
+    where: { id: 1 },
+    update: {},
+    create: {
+      id: 1,
+      name: '展展',
+      url: '展展URL',
+      imgUrl: '展展imgUrl',
+      follower: {
+        connect: [{ id: user1.id }, { id: user2.id }],
+      },
+      videos: {
+        connectOrCreate: [
+          {
+            where: { id: 1 },
+            create: {
+              id: 1,
+              title: '展展的第一支影片',
+              url: '展展的第一支影片URL',
+              imgUrl: '展展的第一支影片imgUrl',
+            },
+          },
+          {
+            where: { id: 2 },
+            create: {
+              id: 2,
+              title: '展展的第二支影片',
+              url: '展展的第二支影片URL',
+              imgUrl: '展展的第二支影片imgUrl',
+            },
+          },
+        ],
+      },
+    },
+  });
+
+  // 如果是關聯的話，要用videoId或是video 會變成optional，因為兩種寫法都可以
+  const comment1 = await prisma.comment.upsert({
+    where: { id: 1 },
+    update: {},
+    create: {
+      id: 1,
+      content: '展展的第一支影片的第一則留言',
+      senderId: user1.id,
+      videoId: 1,
+      children: {
+        connectOrCreate: [
+          {
+            where: { id: 2 },
+            create: {
+              id: 2,
+              content: 'QWQ',
+              senderId: user2.id,
+              videoId: 1,
+            },
+          },
+          {
+            where: { id: 3 },
+            create: {
+              id: 3,
+              content: 'QWQQQQQQ',
+              senderId: user2.id,
+              videoId: 1,
+            },
+          },
+        ],
+      },
+      parentId: null,
+    },
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    // close the Prisma Client at the end
+    await prisma.$disconnect();
+  });
